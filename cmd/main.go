b82bfa4a598b1a537626969ac501d16acbb381e0
Hunter Wiginton <hackastak@HackastakMac.local>
2024-03-24 21:24:24 -0500

    Setup Turso database and complete configuration for connecting to db

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"database/sql"
 	"io"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	_ "github.com/tursodatabase/libsql-client-go/libsql"
+	_"github.com/tursodatabase/libsql-client-go/libsql"
 
 )
 
@@ -27,17 +28,49 @@ func newTemplate() *Templates {
 }
 
 type Pal struct {
-	Key int
+	ID int
 	Name string
-	ImgUrl string
+	Key string
+}
+
+var pals []Pal
+
+func queryPals(db *sql.DB) {
+	rows, err := db.Query("SELECT * FROM pals")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to execute query: %v\n", err)
+		os.Exit(1)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var pal Pal
+
+		if err := rows.Scan(&pal.ID, &pal.Name, &pal.Key); err != nil {
+			fmt.Println("Error scanning row: ", err)
+			return
+		}
+
+		pals = append(pals, pal)
+		fmt.Printf("%d: %s -- %s\n", pal.ID, pal.Name, pal.Key)
+	}
+
+	if err := rows.Err(); err != nil {
+		fmt.Println("Error during rows iteration: ", err)
+	}
+
 }
 
 
-
 func main () {
-	fmt.Println("Hey! It's fuck you buddy!")
-	tursoUrl := "libsql://paldeck-hackastak.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTA3MzE0NDcsImlkIjoiOWZiYmVjZWItMWI2ZC00Yzg0LTgzNmMtYjRlYmZlMmQxMTc4In0.pIJD1pTJYGvJX9sb_Z3iuwqKJuJrYwFuldj32k1uIRMwjUlmd1-lInXmjiY8oGJWzO5RUWYXvtcANgGd14y1BA"
 
+	os.Setenv("PALDECK_TURSO_AUTH_URL", "libsql://paldeck-hackastak.turso.io")
+	os.Setenv("PALDECK_TURSO_AUTH_TOKEN", "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTExOTU2NzksImlkIjoiOWZiYmVjZWItMWI2ZC00Yzg0LTgzNmMtYjRlYmZlMmQxMTc4In0._3Xa4Rc1fBiOgBTH6JGAZ8QbO0_z-szhc6rm2v0zW9HaPRpvcOl052X4sdPlEI8MZOIBOmlpB95_KiLud-U3CA")
+	dbUrl := os.Getenv("PALDECK_TURSO_AUTH_URL")
+	dbAuthToken := os.Getenv("PALDECK_TURSO_AUTH_TOKEN")
+
+	// Turso Database connection configuration
+	tursoUrl := dbUrl+"?authToken="+dbAuthToken
 	db, err := sql.Open("libsql", tursoUrl)
   if err != nil {
     fmt.Fprintf(os.Stderr, "failed to open db %s: %s", tursoUrl, err)
@@ -45,11 +78,14 @@ func main () {
   }
   defer db.Close()
 
+	queryPals(db)
+	
+
 
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	pal := Pal {Name: "Rubio"}
+	pal := pals[0]
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/hackastak/paldeck
 
 go 1.22.0
 
-require github.com/labstack/echo/v4 v4.11.4
+require (
+	github.com/labstack/echo/v4 v4.11.4
+	github.com/tursodatabase/libsql-client-go v0.0.0-20240319161759-27d97b27f9e1
+)
 
 require (
 	github.com/antlr/antlr4/runtime/Go/antlr/v4 v4.0.0-20230512164433-5d1fd1a340c9 // indirect
@@ -12,7 +15,6 @@ require (
 	github.com/libsql/sqlite-antlr4-parser v0.0.0-20230802215326-5cb5bb604475 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/tursodatabase/libsql-client-go v0.0.0-20240220085343-4ae0eb9d0898 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
